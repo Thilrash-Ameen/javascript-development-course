a28c1c12271245f4dccd373557e4bee278983ab3
Thilrash-Ameen <thilrashameen@gmail.com>
2021-07-16 22:26:45 +0530

    javascript-development-course commit

---
+++ operators.js
@@ -0,0 +1,33 @@
+// JavaScript operators
+
+// assign values with equal sign operators
+let x = 5;
+let y = 2;
+
+// addition
+let z = x + y;
+
+// multiplication
+z = x * y;
+
+// substration
+z = x - y; 
+
+// division
+z = x / y;
+
+// increment by 1
+z++;
+
+// decrement by 1
+z--;
+
+// modulus
+z = x % y;
+
+// exponentation - means Math.pow(x,y)
+z = x ** y;
+
+console.log(z);
+
+// As
--- variables.js
+++ variables.js
@@ -1,3 +1,33 @@
-// Variable
+// Variables
 let name = 'Thilrash';
 console.log(name);
+
+var $name = "Thilrash Ameen";
+console.log($name);
+
+const _name = 31.4014;
+console.log(_name);
+
+// let & const not allowed to hoisting but can use in var
+
+// primitive types/ value
+let pname = "Thilrash"; // String Literals
+let age = 24; // Number Literals
+let isApproved = false; // Boolean Literals
+let firstName = undefined;
+let selectedColor = null;
+
+let person = {
+    person_name: "Thilrash Ameen",
+    person_age:25
+};
+
+console.log(person);
+
+// dot notation
+person.person_name = "Jack Reacher";
+console.log(person.person_name);
+
+// bracket notation
+person['person_name'] = "Jhon Doe";
+console.log(person.person_name);
